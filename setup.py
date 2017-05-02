a186119e4586a44f296127617c6635b52db941da
Christian <brenninc@cs.man.ac.uk>
2017-05-02 16:30:31 +0100

    Added setup and requirements files

---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup
+from collections import defaultdict
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+    # scipy must be added in config.py as a mock
+    install_requires = ['sPyNNaker8 >= 1!4.0.0a2',
+                        'sPyNNakerExternalDevicesPlugin == 1!4.0.0a3',
+                        'pynn>=0.8, <0.9']
+else:
+    install_requires = ['sPyNNaker8 >= 1!4.0.0a2',
+                        'sPyNNakerExternalDevicesPlugin == 1!4.0.0a3',
+                        'pyNN >= 0.7, < 0.8']
+
+# Build a list of all project modules, as well as supplementary files
+main_package = "spynnaker8_external_devices_plugin"
+data_extensions = {".aplx", ".xml", ".json", ".xsd"}
+config_extensions = {".cfg", ".template"}
+main_package_dir = os.path.join(os.path.dirname(__file__), main_package)
+start = len(main_package_dir)
+packages = []
+package_data = defaultdict(list)
+for dirname, dirnames, filenames in os.walk(main_package_dir):
+    if '__init__.py' in filenames:
+        package = "{}{}".format(
+            main_package, dirname[start:].replace(os.sep, '.'))
+        packages.append(package)
+    for filename in filenames:
+        _, ext = os.path.splitext(filename)
+        if ext in data_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append("*{}".format(ext))
+            break
+        if ext in config_extensions:
+            package = "{}{}".format(
+                main_package, dirname[start:].replace(os.sep, '.'))
+            package_data[package].append(filename)
+
+url = "https://github.com/SpiNNakerManchester/sPyNNaker8ExternalDevicesPlugin"
+
+setup(
+    name="sPyNNaker8ExternalDevicesPlugin",
+    version="1!4.0.0a3",
+    description="Spinnaker External Devices Plugin extended for PyNN8",
+    url=url,
+    packages=packages,
+    package_data=package_data,
+    install_requires=install_requires
+)
